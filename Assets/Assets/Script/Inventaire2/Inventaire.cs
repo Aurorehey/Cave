56d88ac455d208401c69032be8b183fb21ab1809
Aurorehey <48832067+Aurorehey@users.noreply.github.com>
2020-02-04 16:57:33 +0100

    travaille inventaire du 4.02.2020

---
+++ Assets/Assets/Script/Inventaire2/Inventaire.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class Inventaire : MonoBehaviour
+{
+    bool activation = false;
+    //public GameObject Player;
+    GameObject ObjetarecupererImageGraphi;
+    public Sprite[] image;
+
+
+    void Start()
+    {
+        GetComponent<Canvas>().enabled = false;
+        ObjetarecupererImageGraphi = transform.GetChild(0).gameObject;
+        image = new Sprite[ObjetarecupererImageGraphi.transform.childCount];
+    }
+   
+   
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            activation = !activation;
+
+            //if (!activation)
+            //{
+            //    Player.GetComponent<ThirdPersonController> ().enabled = true;
+            //}
+            //else
+            //{
+            //    Player.GetComponent<ThirdPersonController> ().enabled = false;
+            //    Cursor.lockState = CursorLockMode.None;
+            //    Cursor.visible = true;
+            //}
+
+            GetComponent<Canvas>().enabled = activation;
+
+        }
+
+    }
+
+    
+}
+  
